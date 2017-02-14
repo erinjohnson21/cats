9f38807d8e190961ccb06509536ba5e40e6b1181
Erin Johnson <erinjohnson21@gmail.com>
2017-02-13 21:55:33 -0600

    Added buttons at bottom

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -3,11 +3,11 @@ $.ajax({
   // url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7911c0a69e332abff43825a0b5d6b8&tags=cats&animal&extras=url_m&format=json",
   dataType: "jsonp",
   data: {
-    tags : 'cat, cats, animal, photograph, kitty, kitten',
+    tags : 'cat, animal, photograph, kitten',
     tag_mode : 'all',
     safe_search: 1,
     content_type: 1,
-    per_page: 30
+    per_page: 32,
   },
   jsonp: 'jsonFlickrApi',
 });
