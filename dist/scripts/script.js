4db6dc59a401de5476e83a60ce7b34cb9c9e5bb8
Erin Johnson <erinjohnson21@gmail.com>
2017-02-18 19:48:03 -0600

    Added pagination & smooth scroll

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,9 +1,10 @@
+//Initial Call
 $.ajax({
   url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7911c0a69e332abff43825a0b5d6b8&extras=url_m&format=json",
   // url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7911c0a69e332abff43825a0b5d6b8&tags=cats&animal&extras=url_m&format=json",
   dataType: "jsonp",
   data: {
-    tags : 'cat, animal, photograph, kitten',
+    tags : 'cat, animal, kitten',
     tag_mode : 'all',
     safe_search: 1,
     content_type: 1,
@@ -23,35 +24,51 @@ function jsonFlickrApi(data) {
   $('.gallery').html(kittyPics);
 
 
-  //offset photos so they are more centered
-  //I know this is probably wrong but it's a start
-  // var width = document.getElementsByClassName('images').width;
-  // var height = document.getElementsByClassName('images').height;
-  // function offsets () {
-  //   if (width >= 500) {
-  //     offsetX = width/2;
-  //   } else if (height >= 500) {
-  //     offsetY = height/2;
-  //   }
-  // }
-
-
-  //loop through each page?
-  //move these into their own functions
   var pageNum = data.photos.page;
-  console.log(data.photos.pages)
   $('#right-btn').click(function() {
     if(pageNum !== data.photos.pages) {
-      //move to the next page
       pageNum += 1;
-      console.log(pageNum);
+      $.ajax({
+        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7911c0a69e332abff43825a0b5d6b8&extras=url_m&format=json",
+        dataType: "jsonp",
+        data: {
+          tags : 'cat, animal, kitten',
+          tag_mode : 'all',
+          safe_search: 1,
+          content_type: 1,
+          per_page: 32,
+          page: pageNum
+        },
+        jsonp: 'jsonFlickrApi',
+      });
+      scrollToTop();
     }
   });
   $('#left-btn').click(function() {
     if(pageNum !== 1) {
-      //move to the previous page
       pageNum -= 1;
-      console.log(pageNum);
+      $.ajax({
+        url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9a7911c0a69e332abff43825a0b5d6b8&extras=url_m&format=json",
+        dataType: "jsonp",
+        data: {
+          tags : 'cat, animal, kitten',
+          tag_mode : 'all',
+          safe_search: 1,
+          content_type: 1,
+          per_page: 32,
+          page: pageNum
+        },
+        jsonp: 'jsonFlickrApi',
+      });
+      scrollToTop();
     }
   });
+}
+var timeOut;
+function scrollToTop() {
+  if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
+    window.scrollBy(0,-50);
+    timeOut=setTimeout('scrollToTop()',15);
+  }
+  else clearTimeout(timeOut);
 }
